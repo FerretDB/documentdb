99efe6b6a234dbe1f0d08623c14cfde01b81c7d2
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2025-03-03 22:55:31 +0400

    Update `defineversion` logic (#23)

--- ferretdb_packaging/defineversion/docker.go
+++ ferretdb_packaging/defineversion/docker.go
@@ -1,22 +1,7 @@
-// Copyright 2021 FerretDB Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
 	"fmt"
-	"regexp"
 	"slices"
 	"strings"
 	"text/tabwriter"
@@ -30,11 +15,8 @@ type images struct {
 	productionImages  []string
 }
 
-// pgVer is the version of PostgreSQL.
-var pgVer = regexp.MustCompile(`^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)$`)
-
-// defineDockerTags extracts Docker image names and tags from the environment variables defined by GitHub Actions.
-func defineDockerTags(getenv githubactions.GetenvFunc) (*images, error) {
+// defineDockerVersion extracts Docker image names and tags from the environment variables defined by GitHub Actions.
+func defineDockerVersion(controlDefaultVersion, pgVersion string, getenv githubactions.GetenvFunc) (*images, error) {
 	repo := getenv("GITHUB_REPOSITORY")
 
 	// to support GitHub forks
@@ -51,40 +33,28 @@ func defineDockerTags(getenv githubactions.GetenvFunc) (*images, error) {
 	switch event := getenv("GITHUB_EVENT_NAME"); event {
 	case "pull_request", "pull_request_target":
 		branch := strings.ToLower(getenv("GITHUB_HEAD_REF"))
-		res = defineForPR(owner, repo, branch)
+		res = defineDockerVersionForPR(owner, repo, branch)
 
 	case "push", "schedule", "workflow_run":
 		refName := strings.ToLower(getenv("GITHUB_REF_NAME"))
 
 		switch refType := strings.ToLower(getenv("GITHUB_REF_TYPE")); refType {
 		case "branch":
-			res, err = defineForBranch(owner, repo, refName)
+			res, err = defineDockerVersionForBranch(owner, repo, refName)
 
 		case "tag":
-			var major, minor, patch, prerelease string
-			if major, minor, patch, prerelease, err = semVar(refName); err != nil {
+			var major, minor, patch int
+			var prerelease string
+			if major, minor, patch, prerelease, err = parseGitTag(refName); err != nil {
 				return nil, err
 			}
 
-			pgVersion := getenv("INPUT_PG_VERSION")
-			pgMatch := pgVer.FindStringSubmatch(pgVersion)
-			if pgMatch == nil || len(pgMatch) != pgVer.NumSubexp()+1 {
-				return nil, fmt.Errorf("unexpected PostgreSQL version %q", pgVersion)
-			}
-
-			pgMajor := pgMatch[pgVer.SubexpIndex("major")]
-			pgMinor := pgMatch[pgVer.SubexpIndex("minor")]
-
 			tags := []string{
-				fmt.Sprintf("%s-%s.%s.%s-%s", pgMajor, major, minor, patch, prerelease),
+				fmt.Sprintf("%s-%d.%d.%d-%s", pgVersion, major, minor, patch, prerelease),
 				"latest",
 			}
 
-			if pgMinor != "" {
-				tags = append(tags, fmt.Sprintf("%s.%s-%s.%s.%s-%s", pgMajor, pgMinor, major, minor, patch, prerelease))
-			}
-
-			res = defineForTag(owner, repo, tags)
+			res = defineDockerVersionForTag(owner, repo, tags)
 
 		default:
 			err = fmt.Errorf("unhandled ref type %q for event %q", refType, event)
@@ -108,8 +78,8 @@ func defineDockerTags(getenv githubactions.GetenvFunc) (*images, error) {
 	return res, nil
 }
 
-// defineForPR defines Docker image names and tags for pull requests.
-func defineForPR(owner, repo, branch string) *images {
+// defineDockerVersionForPR defines Docker image names and tags for pull requests.
+func defineDockerVersionForPR(owner, repo, branch string) *images {
 	// for branches like "dependabot/submodules/XXX"
 	parts := strings.Split(branch, "/")
 	branch = parts[len(parts)-1]
@@ -125,8 +95,8 @@ func defineForPR(owner, repo, branch string) *images {
 	return res
 }
 
-// defineForBranch defines Docker image names and tags for branch builds.
-func defineForBranch(owner, repo, branch string) (*images, error) {
+// defineDockerVersionForBranch defines Docker image names and tags for branch builds.
+func defineDockerVersionForBranch(owner, repo, branch string) (*images, error) {
 	if branch != "ferretdb" {
 		return nil, fmt.Errorf("unhandled branch %q", branch)
 	}
@@ -147,14 +117,14 @@ func defineForBranch(owner, repo, branch string) (*images, error) {
 		return res, nil
 	}
 
-	//res.developmentImages = append(res.developmentImages, "quay.io/ferretdb/postgres-documentdb-dev:ferretdb")
-	//res.developmentImages = append(res.developmentImages, "ferretdb/postgres-documentdb-dev:ferretdb")
+	// res.developmentImages = append(res.developmentImages, "quay.io/ferretdb/postgres-documentdb-dev:ferretdb")
+	// res.developmentImages = append(res.developmentImages, "ferretdb/postgres-documentdb-dev:ferretdb")
 
 	return res, nil
 }
 
-// defineForTag defines Docker image names and tags for prerelease tag builds.
-func defineForTag(owner, repo string, tags []string) *images {
+// defineDockerVersionForTag defines Docker image names and tags for prerelease tag builds.
+func defineDockerVersionForTag(owner, repo string, tags []string) *images {
 	res := new(images)
 
 	for _, t := range tags {
@@ -183,34 +153,8 @@ func defineForTag(owner, repo string, tags []string) *images {
 	return res
 }
 
-// setDockerTagsResults sets action output parameters, summary, etc.
-func setDockerTagsResults(action *githubactions.Action, res *images) {
-	var buf strings.Builder
-	w := tabwriter.NewWriter(&buf, 1, 1, 1, ' ', tabwriter.Debug)
-	fmt.Fprintf(w, "\tType\tImage\t\n")
-	fmt.Fprintf(w, "\t----\t-----\t\n")
-
-	for _, image := range res.developmentImages {
-		u := imageURL(image)
-		_, _ = fmt.Fprintf(w, "\tDevelopment\t[`%s`](%s)\t\n", image, u)
-	}
-
-	for _, image := range res.productionImages {
-		u := imageURL(image)
-		_, _ = fmt.Fprintf(w, "\tProduction\t[`%s`](%s)\t\n", image, u)
-	}
-
-	_ = w.Flush()
-
-	action.AddStepSummary(buf.String())
-	action.Infof("%s", buf.String())
-
-	action.SetOutput("development_images", strings.Join(res.developmentImages, ","))
-	action.SetOutput("production_images", strings.Join(res.productionImages, ","))
-}
-
-// imageURL returns HTML page URL for the given image name and tag.
-func imageURL(name string) string {
+// dockerImageURL returns HTML page URL for the given image name and tag.
+func dockerImageURL(name string) string {
 	switch {
 	case strings.HasPrefix(name, "ghcr.io/"):
 		return fmt.Sprintf("https://%s", name)
@@ -222,4 +166,27 @@ func imageURL(name string) string {
 
 	// there is no easy way to get Docker Hub URL for the given tag
 	return fmt.Sprintf("https://hub.docker.com/r/%s/tags", name)
+}
+
+// dockerSummary sets action summary.
+func dockerSummary(action *githubactions.Action, version *images) {
+	var buf strings.Builder
+	w := tabwriter.NewWriter(&buf, 1, 1, 1, ' ', tabwriter.Debug)
+	fmt.Fprintf(w, "\tType\tImage\t\n")
+	fmt.Fprintf(w, "\t----\t-----\t\n")
+
+	for _, image := range version.developmentImages {
+		u := dockerImageURL(image)
+		_, _ = fmt.Fprintf(w, "\tDevelopment\t[`%s`](%s)\t\n", image, u)
+	}
+
+	for _, image := range version.productionImages {
+		u := dockerImageURL(image)
+		_, _ = fmt.Fprintf(w, "\tProduction\t[`%s`](%s)\t\n", image, u)
+	}
+
+	_ = w.Flush()
+
+	action.AddStepSummary(buf.String())
+	action.Infof("%s", buf.String())
 }
--- ferretdb_packaging/defineversion/main.go
+++ ferretdb_packaging/defineversion/main.go
@@ -1,18 +1,4 @@
-// Copyright 2021 FerretDB Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Package main defines debian version number of DocumentDB for CI builds.
+// Package main defines version numbers for DocumentDB.
 package main
 
 import (
@@ -21,66 +7,52 @@ import (
 	"os"
 	"regexp"
 	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/sethvargo/go-githubactions"
 )
 
-func main() {
-	commandF := flag.String("command", "", "command to run, possible values: [deb-version, docker-tags]")
-
-	controlFileF := flag.String("control-file", "../pg_documentdb/documentdb.control", "pg_documentdb/documentdb.control file path")
-
-	flag.Parse()
-
-	action := githubactions.New()
-
-	debugEnv(action)
-
-	if *commandF == "" {
-		action.Fatalf("-command flag is empty.")
-	}
-
-	switch *commandF {
-	case "deb-version":
-		if *controlFileF == "" {
-			action.Fatalf("%s", "-control-file flag is empty.")
-		}
-
-		controlDefaultVersion, err := getControlDefaultVersion(*controlFileF)
-		if err != nil {
-			action.Fatalf("%s", err)
-		}
-
-		packageVersion, err := definePackageVersion(controlDefaultVersion, action.Getenv)
-		if err != nil {
-			action.Fatalf("%s", err)
-		}
-
-		setDebianVersionResults(action, packageVersion)
-	case "docker-tags":
-		res, err := defineDockerTags(action.Getenv)
-		if err != nil {
-			action.Fatalf("%s", err)
-		}
-
-		setDockerTagsResults(action, res)
-	default:
-		action.Fatalf("unhandled command %q", *commandF)
-	}
-}
-
-// controlDefaultVer matches major, minor and "patch" from default_version field in control file,
-// see pg_documentdb_core/documentdb_core.control.
-var controlDefaultVer = regexp.MustCompile(`(?m)^default_version = '(?P<major>[0-9]+)\.(?P<minor>[0-9]+)-(?P<patch>[0-9]+)'$`)
-
 // semVerTag is a https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string,
 // but with a leading `v`.
 var semVerTag = regexp.MustCompile(`^v(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
 
-// disallowedVer matches disallowed characters of Debian `upstream_version` when used without `debian_revision`.
-// See https://www.debian.org/doc/debian-policy/ch-controlfields.html#version.
-var disallowedVer = regexp.MustCompile(`[^A-Za-z0-9~.+]`)
+// parseGitTag parses git tag in specific format and returns SemVer components.
+//
+// Expected format is v0.100.0-ferretdb-2.0.0-rc.2,
+// where v0.100.0 is a DocumentDB version (0.100-0 -> 0.100.0),
+// and ferretdb-2.0.0-rc.2 is a compatible FerretDB version.
+func parseGitTag(tag string) (major, minor, patch int, prerelease string, err error) {
+	match := semVerTag.FindStringSubmatch(tag)
+	if match == nil || len(match) != semVerTag.NumSubexp()+1 {
+		err = fmt.Errorf("unexpected git tag format %q", tag)
+		return
+	}
+
+	if major, err = strconv.Atoi(match[semVerTag.SubexpIndex("major")]); err != nil {
+		return
+	}
+	if minor, err = strconv.Atoi(match[semVerTag.SubexpIndex("minor")]); err != nil {
+		return
+	}
+	if patch, err = strconv.Atoi(match[semVerTag.SubexpIndex("patch")]); err != nil {
+		return
+	}
+	prerelease = match[semVerTag.SubexpIndex("prerelease")]
+	buildmetadata := match[semVerTag.SubexpIndex("buildmetadata")]
+
+	if !strings.HasPrefix(prerelease, "ferretdb-") {
+		err = fmt.Errorf("prerelease %q should start with 'ferretdb-'", prerelease)
+		return
+	}
+
+	if buildmetadata != "" {
+		err = fmt.Errorf("buildmetadata %q is present", buildmetadata)
+		return
+	}
+
+	return
+}
 
 // debugEnv logs all environment variables that start with `GITHUB_` or `INPUT_`
 // in debug level.
@@ -102,139 +74,66 @@ func debugEnv(action *githubactions.Action) {
 	}
 }
 
-// getControlDefaultVersion returns the default_version field from the control file
-// in SemVer format (0.100-0 -> 0.100.0).
-func getControlDefaultVersion(f string) (string, error) {
-	b, err := os.ReadFile(f)
+// defineVersion returns Debian package version and Docker tags.
+func defineVersion(controlDefaultVersion, pgVersion string, getenv githubactions.GetenvFunc) (string, *images, error) {
+	debian, err := defineDebianVersion(controlDefaultVersion, pgVersion, getenv)
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
 
-	match := controlDefaultVer.FindSubmatch(b)
-	if match == nil || len(match) != controlDefaultVer.NumSubexp()+1 {
-		return "", fmt.Errorf("control file did not find default_version: %s", f)
+	docker, err := defineDockerVersion(controlDefaultVersion, pgVersion, getenv)
+	if err != nil {
+		return "", nil, err
 	}
 
-	major := match[controlDefaultVer.SubexpIndex("major")]
-	minor := match[controlDefaultVer.SubexpIndex("minor")]
-	patch := match[controlDefaultVer.SubexpIndex("patch")]
-
-	return fmt.Sprintf("%s.%s.%s", major, minor, patch), nil
+	return debian, docker, nil
 }
 
-// definePackageVersion returns valid Debian package version,
-// based on `default_version` in the control file and environment variables set by GitHub Actions.
-//
-// See https://www.debian.org/doc/debian-policy/ch-controlfields.html#version.
-// We use `upstream_version` only.
-// For that reason, we can't use `-`, so we replace it with `~`.
-func definePackageVersion(controlDefaultVersion string, getenv githubactions.GetenvFunc) (string, error) {
-	var packageVersion string
-	var err error
+func main() {
+	controlFileF := flag.String("control-file", "../../pg_documentdb/documentdb.control", "pg_documentdb/documentdb.control file path")
+	pgVersionF := flag.String("pg-version", "17", "Major PostgreSQL version")
 
-	switch event := getenv("GITHUB_EVENT_NAME"); event {
-	case "pull_request", "pull_request_target":
-		branch := strings.ToLower(getenv("GITHUB_HEAD_REF"))
-		packageVersion = definePackageVersionForPR(controlDefaultVersion, branch)
+	flag.Parse()
 
-	case "push", "schedule", "workflow_run":
-		refName := strings.ToLower(getenv("GITHUB_REF_NAME"))
+	action := githubactions.New()
 
-		switch refType := strings.ToLower(getenv("GITHUB_REF_TYPE")); refType {
-		case "branch":
-			packageVersion, err = definePackageVersionForBranch(controlDefaultVersion, refName)
+	debugEnv(action)
 
-		case "tag":
-			packageVersion, err = definePackagerVersionForTag(refName)
+	if *controlFileF == "" {
+		action.Fatalf("%s", "-control-file flag is empty.")
+	}
 
-		default:
-			err = fmt.Errorf("unhandled ref type %q for event %q", refType, event)
-		}
-
+	switch *pgVersionF {
+	case "15", "16", "17":
+		// nothing
 	default:
-		err = fmt.Errorf("unhandled event type %q", event)
+		action.Fatalf("%s", fmt.Sprintf("Invalid PostgreSQL version %q.", *pgVersionF))
 	}
 
+	controlDefaultVersion, err := getControlDefaultVersion(*controlFileF)
 	if err != nil {
-		return "", err
+		action.Fatalf("%s", err)
 	}
 
-	if packageVersion == "" {
-		return "", fmt.Errorf("both packageVersion and err are nil")
-	}
-
-	return packageVersion, nil
-}
-
-// definePackageVersionForPR returns valid Debian package version for PR.
-// See [definePackageVersion].
-func definePackageVersionForPR(controlDefaultVersion, branch string) string {
-	// for branches like "dependabot/submodules/XXX"
-	parts := strings.Split(branch, "/")
-	branch = parts[len(parts)-1]
-	res := fmt.Sprintf("%s-pr-%s", controlDefaultVersion, branch)
-
-	return disallowedVer.ReplaceAllString(res, "~")
-}
-
-// definePackageVersionForBranch returns valid Debian package version for branch.
-// See [definePackageVersion].
-func definePackageVersionForBranch(controlDefaultVersion, branch string) (string, error) {
-	switch branch {
-	case "ferretdb":
-		return fmt.Sprintf("%s~branch~%s", controlDefaultVersion, branch), nil
-	default:
-		return "", fmt.Errorf("unhandled branch %q", branch)
-	}
-}
-
-// semVar parses tag and returns version components.
-//
-// It returns error for invalid tag syntax, prerelease is missing `ferretdb` or if it has buildmetadata.
-func semVar(tag string) (major, minor, patch, prerelease string, err error) {
-	match := semVerTag.FindStringSubmatch(tag)
-	if match == nil || len(match) != semVerTag.NumSubexp()+1 {
-		return "", "", "", "", fmt.Errorf("unexpected tag syntax %q", tag)
-	}
-
-	major = match[semVerTag.SubexpIndex("major")]
-	minor = match[semVerTag.SubexpIndex("minor")]
-	patch = match[semVerTag.SubexpIndex("patch")]
-	prerelease = match[semVerTag.SubexpIndex("prerelease")]
-	buildmetadata := match[semVerTag.SubexpIndex("buildmetadata")]
-
-	if prerelease == "" {
-		return "", "", "", "", fmt.Errorf("prerelease is empty")
-	}
-
-	if !strings.Contains(prerelease, "ferretdb") {
-		return "", "", "", "", fmt.Errorf("prerelease %q should include `ferretdb`", prerelease)
-	}
-
-	if buildmetadata != "" {
-		return "", "", "", "", fmt.Errorf("buildmetadata %q is present", buildmetadata)
-	}
-
-	return
-}
-
-// definePackagerVersionForTag returns valid Debian package version for tag.
-// See [definePackageVersion].
-func definePackagerVersionForTag(tag string) (string, error) {
-	major, minor, patch, prerelease, err := semVar(tag)
+	debian, docker, err := defineVersion(controlDefaultVersion, *pgVersionF, action.Getenv)
 	if err != nil {
-		return "", err
+		action.Fatalf("%s", err)
 	}
 
-	res := fmt.Sprintf("%s.%s.%s-%s", major, minor, patch, prerelease)
-	return disallowedVer.ReplaceAllString(res, "~"), nil
-}
+	debianSummary(action, debian)
+	action.SetOutput("debian_version", debian)
 
-// setDebianVersionResults sets action output parameters, summary, etc.
-func setDebianVersionResults(action *githubactions.Action, res string) {
-	output := fmt.Sprintf("version: `%s`", res)
+	dockerSummary(action, docker)
 
-	action.AddStepSummary(output)
-	action.Infof("%s", output)
-	action.SetOutput("version", res)
+	developmentTagFlags := make([]string, len(docker.developmentImages))
+	for i, image := range docker.developmentImages {
+		developmentTagFlags[i] = fmt.Sprintf("--tag %s", image)
+	}
+	action.SetOutput("docker_development_tag_flags", strings.Join(developmentTagFlags, " "))
+
+	productionTagFlags := make([]string, len(docker.productionImages))
+	for i, image := range docker.productionImages {
+		productionTagFlags[i] = fmt.Sprintf("--tag %s", image)
+	}
+	action.SetOutput("docker_production_tag_flags", strings.Join(productionTagFlags, " "))
 }
