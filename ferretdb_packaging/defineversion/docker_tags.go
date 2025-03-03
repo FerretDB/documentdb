c9c13d6881d9172c1f2feff3ffd6f129e88a47cc
Chi Fujii <chi.fujii@ferretdb.io>
2025-03-03 22:14:28 +0900

    Define Docker build tags (#16)
    Closes FerretDB/FerretDB#4725.

---
+++ ferretdb_packaging/defineversion/docker_tags.go
@@ -0,0 +1,225 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"slices"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/sethvargo/go-githubactions"
+)
+
+// images represents Docker image names and tags extracted from the environment.
+type images struct {
+	developmentImages []string
+	productionImages  []string
+}
+
+// pgVer is the version of PostgreSQL.
+var pgVer = regexp.MustCompile(`^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)$`)
+
+// defineDockerTags extracts Docker image names and tags from the environment variables defined by GitHub Actions.
+func defineDockerTags(getenv githubactions.GetenvFunc) (*images, error) {
+	repo := getenv("GITHUB_REPOSITORY")
+
+	// to support GitHub forks
+	parts := strings.Split(strings.ToLower(repo), "/")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("failed to split %q into owner and name", repo)
+	}
+	owner := parts[0]
+	repo = parts[1]
+
+	var res *images
+	var err error
+
+	switch event := getenv("GITHUB_EVENT_NAME"); event {
+	case "pull_request", "pull_request_target":
+		branch := strings.ToLower(getenv("GITHUB_HEAD_REF"))
+		res = defineForPR(owner, repo, branch)
+
+	case "push", "schedule", "workflow_run":
+		refName := strings.ToLower(getenv("GITHUB_REF_NAME"))
+
+		switch refType := strings.ToLower(getenv("GITHUB_REF_TYPE")); refType {
+		case "branch":
+			res, err = defineForBranch(owner, repo, refName)
+
+		case "tag":
+			var major, minor, patch, prerelease string
+			if major, minor, patch, prerelease, err = semVar(refName); err != nil {
+				return nil, err
+			}
+
+			pgVersion := getenv("INPUT_PG_VERSION")
+			pgMatch := pgVer.FindStringSubmatch(pgVersion)
+			if pgMatch == nil || len(pgMatch) != pgVer.NumSubexp()+1 {
+				return nil, fmt.Errorf("unexpected PostgreSQL version %q", pgVersion)
+			}
+
+			pgMajor := pgMatch[pgVer.SubexpIndex("major")]
+			pgMinor := pgMatch[pgVer.SubexpIndex("minor")]
+
+			tags := []string{
+				fmt.Sprintf("%s-%s.%s.%s-%s", pgMajor, major, minor, patch, prerelease),
+				"latest",
+			}
+
+			if pgMinor != "" {
+				tags = append(tags, fmt.Sprintf("%s.%s-%s.%s.%s-%s", pgMajor, pgMinor, major, minor, patch, prerelease))
+			}
+
+			res = defineForTag(owner, repo, tags)
+
+		default:
+			err = fmt.Errorf("unhandled ref type %q for event %q", refType, event)
+		}
+
+	default:
+		err = fmt.Errorf("unhandled event type %q", event)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res == nil {
+		return nil, fmt.Errorf("both res and err are nil")
+	}
+
+	slices.Sort(res.developmentImages)
+	slices.Sort(res.productionImages)
+
+	return res, nil
+}
+
+// defineForPR defines Docker image names and tags for pull requests.
+func defineForPR(owner, repo, branch string) *images {
+	// for branches like "dependabot/submodules/XXX"
+	parts := strings.Split(branch, "/")
+	branch = parts[len(parts)-1]
+
+	res := &images{
+		developmentImages: []string{
+			fmt.Sprintf("ghcr.io/%s/postgres-%s-dev:pr-%s", owner, repo, branch),
+		},
+	}
+
+	// PRs are only for testing; no Quay.io and Docker Hub repos
+
+	return res
+}
+
+// defineForBranch defines Docker image names and tags for branch builds.
+func defineForBranch(owner, repo, branch string) (*images, error) {
+	if branch != "ferretdb" {
+		return nil, fmt.Errorf("unhandled branch %q", branch)
+	}
+
+	res := &images{
+		developmentImages: []string{
+			fmt.Sprintf("ghcr.io/%s/postgres-%s-dev:ferretdb", owner, repo),
+		},
+	}
+
+	// forks don't have Quay.io and Docker Hub orgs
+	if owner != "ferretdb" {
+		return res, nil
+	}
+
+	// we don't have Quay.io and Docker Hub repos for other GitHub repos
+	if repo != "documentdb" {
+		return res, nil
+	}
+
+	//res.developmentImages = append(res.developmentImages, "quay.io/ferretdb/postgres-documentdb-dev:ferretdb")
+	//res.developmentImages = append(res.developmentImages, "ferretdb/postgres-documentdb-dev:ferretdb")
+
+	return res, nil
+}
+
+// defineForTag defines Docker image names and tags for prerelease tag builds.
+func defineForTag(owner, repo string, tags []string) *images {
+	res := new(images)
+
+	for _, t := range tags {
+		res.developmentImages = append(res.developmentImages, fmt.Sprintf("ghcr.io/%s/postgres-%s-dev:%s", owner, repo, t))
+		res.productionImages = append(res.productionImages, fmt.Sprintf("ghcr.io/%s/postgres-%s:%s", owner, repo, t))
+	}
+
+	// forks don't have Quay.io and Docker Hub orgs
+	if owner != "ferretdb" {
+		return res
+	}
+
+	// we don't have Quay.io and Docker Hub repos for other GitHub repos
+	if repo != "documentdb" {
+		return res
+	}
+
+	//for _, t := range tags {
+	//	res.developmentImages = append(res.developmentImages, fmt.Sprintf("quay.io/ferretdb/postgres-documentdb-dev:%s", t))
+	//	res.productionImages = append(res.productionImages, fmt.Sprintf("quay.io/ferretdb/postgres-documentdb:%s", t))
+	//
+	//	res.developmentImages = append(res.developmentImages, fmt.Sprintf("ferretdb/postgres-documentdb-dev:%s", t))
+	//	res.productionImages = append(res.productionImages, fmt.Sprintf("ferretdb/postgres-documentdb:%s", t))
+	//}
+
+	return res
+}
+
+// setDockerTagsResults sets action output parameters, summary, etc.
+func setDockerTagsResults(action *githubactions.Action, res *images) {
+	var buf strings.Builder
+	w := tabwriter.NewWriter(&buf, 1, 1, 1, ' ', tabwriter.Debug)
+	fmt.Fprintf(w, "\tType\tImage\t\n")
+	fmt.Fprintf(w, "\t----\t-----\t\n")
+
+	for _, image := range res.developmentImages {
+		u := imageURL(image)
+		_, _ = fmt.Fprintf(w, "\tDevelopment\t[`%s`](%s)\t\n", image, u)
+	}
+
+	for _, image := range res.productionImages {
+		u := imageURL(image)
+		_, _ = fmt.Fprintf(w, "\tProduction\t[`%s`](%s)\t\n", image, u)
+	}
+
+	_ = w.Flush()
+
+	action.AddStepSummary(buf.String())
+	action.Infof("%s", buf.String())
+
+	action.SetOutput("development_images", strings.Join(res.developmentImages, ","))
+	action.SetOutput("production_images", strings.Join(res.productionImages, ","))
+}
+
+// imageURL returns HTML page URL for the given image name and tag.
+func imageURL(name string) string {
+	switch {
+	case strings.HasPrefix(name, "ghcr.io/"):
+		return fmt.Sprintf("https://%s", name)
+	case strings.HasPrefix(name, "quay.io/"):
+		return fmt.Sprintf("https://%s", name)
+	}
+
+	name, _, _ = strings.Cut(name, ":")
+
+	// there is no easy way to get Docker Hub URL for the given tag
+	return fmt.Sprintf("https://hub.docker.com/r/%s/tags", name)
+}
---
+++ ferretdb_packaging/defineversion/docker_tags_test.go
@@ -0,0 +1,488 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"github.com/sethvargo/go-githubactions"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestDefineDockerTags(t *testing.T) {
+	for name, tc := range map[string]struct {
+		env      map[string]string
+		expected *images
+	}{
+		"pull_request": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "ferretdb",
+				"GITHUB_EVENT_NAME": "pull_request",
+				"GITHUB_HEAD_REF":   "docker-tag",
+				"GITHUB_REF_NAME":   "1/merge",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/ferretdb/postgres-documentdb-dev:pr-docker-tag",
+				},
+			},
+		},
+		"pull_request-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "ferretdb",
+				"GITHUB_EVENT_NAME": "pull_request",
+				"GITHUB_HEAD_REF":   "docker-tag",
+				"GITHUB_REF_NAME":   "1/merge",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo-dev:pr-docker-tag",
+				},
+			},
+		},
+
+		"pull_request_target": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "ferretdb",
+				"GITHUB_EVENT_NAME": "pull_request_target",
+				"GITHUB_HEAD_REF":   "docker-tag",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/ferretdb/postgres-documentdb-dev:pr-docker-tag",
+				},
+			},
+		},
+		"pull_request_target-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "ferretdb",
+				"GITHUB_EVENT_NAME": "pull_request_target",
+				"GITHUB_HEAD_REF":   "docker-tag",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo-dev:pr-docker-tag",
+				},
+			},
+		},
+
+		"push/ferretdb": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					//"ferretdb/postgres-documentdb-dev:ferretdb",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:ferretdb",
+					//"quay.io/ferretdb/postgres-documentdb-dev:ferretdb",
+				},
+			},
+		},
+		"push/ferretdb-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo-dev:ferretdb",
+				},
+			},
+		},
+
+		"push/main": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "main",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+		},
+		"push/main-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "main",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+		},
+
+		"push/tag/release": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.102.0-ferretdb",
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16.7",
+			},
+			expected: &images{
+				developmentImages: []string{
+					//"ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb",
+					//"ferretdb/postgres-documentdb-dev:16.7-0.102.0-ferretdb",
+					//"ferretdb/postgres-documentdb-dev:latest",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:16.7-0.102.0-ferretdb",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:latest",
+					//"quay.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb",
+					//"quay.io/ferretdb/postgres-documentdb-dev:16.7-0.102.0-ferretdb",
+					//"quay.io/ferretdb/postgres-documentdb-dev:latest",
+				},
+				productionImages: []string{
+					//"ferretdb/postgres-documentdb:16-0.102.0-ferretdb",
+					//"ferretdb/postgres-documentdb:16.7-0.102.0-ferretdb",
+					//"ferretdb/postgres-documentdb:latest",
+					"ghcr.io/ferretdb/postgres-documentdb:16-0.102.0-ferretdb",
+					"ghcr.io/ferretdb/postgres-documentdb:16.7-0.102.0-ferretdb",
+					"ghcr.io/ferretdb/postgres-documentdb:latest",
+					//"quay.io/ferretdb/postgres-documentdb:16-0.102.0-ferretdb",
+					//"quay.io/ferretdb/postgres-documentdb:16.7-0.102.0-ferretdb",
+					//"quay.io/ferretdb/postgres-documentdb:latest",
+				},
+			},
+		},
+		"push/tag/release-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.102.0-ferretdb",
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16.7",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo-dev:16-0.102.0-ferretdb",
+					"ghcr.io/otherorg/postgres-otherrepo-dev:16.7-0.102.0-ferretdb",
+					"ghcr.io/otherorg/postgres-otherrepo-dev:latest",
+				},
+				productionImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo:16-0.102.0-ferretdb",
+					"ghcr.io/otherorg/postgres-otherrepo:16.7-0.102.0-ferretdb",
+					"ghcr.io/otherorg/postgres-otherrepo:latest",
+				},
+			},
+		},
+
+		"push/tag/release-rc": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.102.0-ferretdb-2.0.0-rc2",
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16.7", // set major and minor version
+			},
+			expected: &images{
+				developmentImages: []string{
+					//"ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb-2.0.0-rc2",
+					//"ferretdb/postgres-documentdb-dev:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					//"ferretdb/postgres-documentdb-dev:latest",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:latest",
+					//"quay.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb-2.0.0-rc2",
+					//"quay.io/ferretdb/postgres-documentdb-dev:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					//"quay.io/ferretdb/postgres-documentdb-dev:latest",
+				},
+				productionImages: []string{
+					//"ferretdb/postgres-documentdb:16-0.102.0-ferretdb-2.0.0-rc2",
+					//"ferretdb/postgres-documentdb:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					//"ferretdb/postgres-documentdb:latest",
+					"ghcr.io/ferretdb/postgres-documentdb:16-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/ferretdb/postgres-documentdb:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/ferretdb/postgres-documentdb:latest",
+					//"quay.io/ferretdb/postgres-documentdb:16-0.102.0-ferretdb-2.0.0-rc2",
+					//"quay.io/ferretdb/postgres-documentdb:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					//"quay.io/ferretdb/postgres-documentdb:latest",
+				},
+			},
+		},
+		"push/tag/release-rc-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.102.0-ferretdb-2.0.0-rc2",
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16.7", // set major and minor version
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo-dev:16-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/otherorg/postgres-otherrepo-dev:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/otherorg/postgres-otherrepo-dev:latest",
+				},
+				productionImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo:16-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/otherorg/postgres-otherrepo:16.7-0.102.0-ferretdb-2.0.0-rc2",
+					"ghcr.io/otherorg/postgres-otherrepo:latest",
+				},
+			},
+		},
+
+		"push/tag/release// missing minor": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.102.0-ferretdb-2.0.0-rc2",
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16", // missing minor
+			},
+		},
+		"push/tag/release-missing-minor-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.102.0-ferretdb-2.0.0-rc2",
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16", // missing minor
+			},
+		},
+
+		"push/tag/wrong": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "0.102.0-ferretdb-2.0.0-rc2", // no leading v
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+		},
+		"push/tag/wrong-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "0.102.0-ferretdb-2.0.0-rc2", // no leading v
+				"GITHUB_REF_TYPE":   "tag",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16",
+			},
+		},
+
+		"schedule": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "schedule",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					//"ferretdb/postgres-documentdb-dev:ferretdb",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:ferretdb",
+					//"quay.io/ferretdb/postgres-documentdb-dev:ferretdb",
+				},
+			},
+		},
+		"schedule-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "schedule",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo-dev:ferretdb",
+				},
+			},
+		},
+
+		"workflow_run": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "workflow_run",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "FerretDB/documentdb",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					//"ferretdb/postgres-documentdb-dev:ferretdb",
+					"ghcr.io/ferretdb/postgres-documentdb-dev:ferretdb",
+					//"quay.io/ferretdb/postgres-documentdb-dev:ferretdb",
+				},
+			},
+		},
+		"workflow_run-other": {
+			env: map[string]string{
+				"GITHUB_BASE_REF":   "",
+				"GITHUB_EVENT_NAME": "workflow_run",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+				"GITHUB_REPOSITORY": "OtherOrg/OtherRepo",
+				"INPUT_PG_VERSION":  "16",
+			},
+			expected: &images{
+				developmentImages: []string{
+					"ghcr.io/otherorg/postgres-otherrepo-dev:ferretdb",
+				},
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			actual, err := defineDockerTags(getEnvFunc(t, tc.env))
+			if tc.expected == nil {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestImageURL(t *testing.T) {
+	// expected URLs should work
+	assert.Equal(
+		t,
+		"https://ghcr.io/ferretdb/postgres-documentdb-dev:pr-docker-tag",
+		imageURL("ghcr.io/ferretdb/postgres-documentdb-dev:pr-docker-tag"),
+	)
+	assert.Equal(
+		t,
+		"https://quay.io/ferretdb/postgres-documentdb-dev:pr-docker-tag",
+		imageURL("quay.io/ferretdb/postgres-documentdb-dev:pr-docker-tag"),
+	)
+	assert.Equal(
+		t,
+		"https://hub.docker.com/r/ferretdb/postgres-documentdb-dev/tags",
+		imageURL("ferretdb/postgres-documentdb-dev:pr-docker-tag"),
+	)
+}
+
+func TestDockerTagsResults(t *testing.T) {
+	dir := t.TempDir()
+
+	summaryF, err := os.CreateTemp(dir, "summary")
+	require.NoError(t, err)
+	defer summaryF.Close()
+
+	outputF, err := os.CreateTemp(dir, "output")
+	require.NoError(t, err)
+	defer outputF.Close()
+
+	var stdout bytes.Buffer
+	getenv := getEnvFunc(t, map[string]string{
+		"GITHUB_STEP_SUMMARY": summaryF.Name(),
+		"GITHUB_OUTPUT":       outputF.Name(),
+	})
+	action := githubactions.New(githubactions.WithGetenv(getenv), githubactions.WithWriter(&stdout))
+
+	result := &images{
+		developmentImages: []string{
+			"ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb",
+			"ghcr.io/ferretdb/postgres-documentdb-dev:latest",
+		},
+		productionImages: []string{
+			"quay.io/ferretdb/postgres-documentdb:latest",
+		},
+	}
+
+	setDockerTagsResults(action, result)
+
+	expectedStdout := strings.ReplaceAll(`
+ |Type        |Image                                                                                                                                  |
+ |----        |-----                                                                                                                                  |
+ |Development |['ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb'](https://ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb) |
+ |Development |['ghcr.io/ferretdb/postgres-documentdb-dev:latest'](https://ghcr.io/ferretdb/postgres-documentdb-dev:latest)                           |
+ |Production  |['quay.io/ferretdb/postgres-documentdb:latest'](https://quay.io/ferretdb/postgres-documentdb:latest)                                   |
+
+`[1:], "'", "`",
+	)
+	assert.Equal(t, expectedStdout, stdout.String(), "stdout does not match")
+
+	expectedSummary := strings.ReplaceAll(`
+ |Type        |Image                                                                                                                                  |
+ |----        |-----                                                                                                                                  |
+ |Development |['ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb'](https://ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb) |
+ |Development |['ghcr.io/ferretdb/postgres-documentdb-dev:latest'](https://ghcr.io/ferretdb/postgres-documentdb-dev:latest)                           |
+ |Production  |['quay.io/ferretdb/postgres-documentdb:latest'](https://quay.io/ferretdb/postgres-documentdb:latest)                                   |
+
+`[1:], "'", "`",
+	)
+	b, err := io.ReadAll(summaryF)
+	require.NoError(t, err)
+	assert.Equal(t, expectedSummary, string(b), "summary does not match")
+
+	expectedOutput := `
+development_images<<_GitHubActionsFileCommandDelimeter_
+ghcr.io/ferretdb/postgres-documentdb-dev:16-0.102.0-ferretdb,ghcr.io/ferretdb/postgres-documentdb-dev:latest
+_GitHubActionsFileCommandDelimeter_
+production_images<<_GitHubActionsFileCommandDelimeter_
+quay.io/ferretdb/postgres-documentdb:latest
+_GitHubActionsFileCommandDelimeter_
+`[1:]
+	b, err = io.ReadAll(outputF)
+	require.NoError(t, err)
+	assert.Equal(t, expectedOutput, string(b), "output parameters does not match")
+}
