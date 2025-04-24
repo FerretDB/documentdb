725a7100ba79cab1db116b2204068fcf7bc2cfc1
Chi Fujii <chi.fujii@ferretdb.io>
2025-04-24 11:37:01 +0900

    Revert "workflow dispatch"
    This reverts commit ea8d75630b8ac02e3d0f8ed613bee492f7e6ff9c.

--- ferretdb_packaging/defineversion/main_test.go
+++ ferretdb_packaging/defineversion/main_test.go
@@ -316,10 +316,10 @@ func TestDefineVersion(t *testing.T) {
 			},
 		},
 
-		"workflow_dispatch": {
+		"workflow_run": {
 			env: map[string]string{
 				"GITHUB_BASE_REF":   "",
-				"GITHUB_EVENT_NAME": "workflow_dispatch",
+				"GITHUB_EVENT_NAME": "workflow_run",
 				"GITHUB_HEAD_REF":   "",
 				"GITHUB_REF_NAME":   "ferretdb",
 				"GITHUB_REF_TYPE":   "branch",
@@ -334,10 +334,10 @@ func TestDefineVersion(t *testing.T) {
 				debian: "0.100.0~ferretdb",
 			},
 		},
-		"workflow_dispatch-other": {
+		"workflow_run-other": {
 			env: map[string]string{
 				"GITHUB_BASE_REF":   "",
-				"GITHUB_EVENT_NAME": "workflow_dispatch",
+				"GITHUB_EVENT_NAME": "workflow_run",
 				"GITHUB_HEAD_REF":   "",
 				"GITHUB_REF_NAME":   "ferretdb",
 				"GITHUB_REF_TYPE":   "branch",
