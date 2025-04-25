700a399ce7d9282867c4edc83eb396411ccab663
Chi Fujii <chi.fujii@ferretdb.io>
2025-04-25 12:12:06 +0900

    documentdb version is required for ferretdb

--- packaging/build_packages.sh
+++ packaging/build_packages.sh
@@ -12,9 +12,9 @@ function show_help {
     echo "Mandatory Arguments:"
     echo "  --os                 OS to build packages for. Possible values: [deb11, deb12, ubuntu22.04, ubuntu24.04]"
     echo "  --pg                 PG version to build packages for. Possible values: [15, 16, 17]"
+    echo "  --version            The version of documentdb to build. Examples: [0.100.0, 0.101.0]"
     echo ""
     echo "Optional Arguments:"
-    echo "  --version            The version of documentdb to build. Examples: [0.100.0, 0.101.0]"
     echo "  --test-clean-install Test installing the packages in a clean Docker container."
     echo "  --output-dir         Relative path from the repo root of the directory where to drop the packages. The directory will be created if it doesn't exist. Default: packaging"
     echo "  -h, --help           Display this help message."
@@ -89,16 +89,10 @@ if [[ -z "$PG" ]]; then
     exit 1
 fi
 
-# get the version from control file
 if [[ -z "$DOCUMENTDB_VERSION" ]]; then
-    DOCUMENTDB_VERSION=$(grep -E "^default_version" pg_documentdb_core/documentdb_core.control | sed -E "s/.*'([0-9]+\.[0-9]+-[0-9]+)'.*/\1/")
-    DOCUMENTDB_VERSION=$(echo $DOCUMENTDB_VERSION | sed "s/-/./g")
-    echo "DOCUMENTDB_VERSION extracted from control file: $DOCUMENTDB_VERSION"
-    if [[ -z "$DOCUMENTDB_VERSION" ]]; then
-        echo "Error: --version is required and could not be found in the control file."
-        show_help
-        exit 1
-    fi
+    echo "Error: --version is required."
+    show_help
+    exit 1
 fi
 
 # Set the appropriate Docker image based on the OS
